da6897fa23114c9478eb8f8effb989bd6b48a25f
Max <max_lisyanskiy@mail.ru>
2020-07-29 19:26:22 +0300

    wp-single_pages

--- single.php
+++ single.php
@@ -1,24 +1,29 @@
 <?php
-/*
-Template Name: Игрушки
-*/
+get_header();
 ?>
+    
+	<main id="primary" class="site-main">
 
-<?php
-    get_header();
-?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
 
 
-<!-- Soft_toys -->
-<div class="toys">
-<div class="container">
-        <h2 class="subtitle">Мягкие игрушки</h2>
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+    <div class="container toys">
+        <h2 class="subtitle">Возможно вам понравиться</h2>
         <div class="toys__wrapper">
 
         <?php
             // параметры по умолчанию
             $posts = get_posts( array(
-                'numberposts' => -1,
+                'numberposts' => 3,
                 'category_name'    => 'soft_toys',
                 'orderby'     => 'date',
                 'order'       => 'ASC',
@@ -52,48 +57,6 @@ Template Name: Игрушки
         ?>
 </div>
 
-
- <!-- edu_toys -->
-        <h2 class="subtitle">Развивающие игрушки</h2>
-        <div class="toys__wrapper">
-
-        <?php
-            // параметры по умолчанию
-            $posts = get_posts( array(
-                'numberposts' => -1,
-                'category_name'    => 'edu_toys',
-                'orderby'     => 'date',
-                'order'       => 'ASC',
-                'post_type'   => 'post',
-                'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
-            ) );
-
-            foreach( $posts as $post ){
-                setup_postdata($post);
-                ?>
-                <div class="toys__item" style="background-image: url(<?php 
-                    if(has_post_thumbnail()) {
-                        the_post_thumbnail_url();
-                    } else {
-                        echo get_template_directory_uri() . '/assets/img/not-found.jpg';
-                    }
-                    ?>)">
-                    <div class="toys__item-info">
-                        <div class="toys__item-title"><?php the_title(); ?></div>
-                        <div class="toys__item-descr">
-                                <?php the_field('toys_descr'); ?>                        
-                        </div>
-                        <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
-                    </div>
-                </div>
-
-            <?php
-            }
-
-            wp_reset_postdata(); // сброс
-        ?>
-        </div>
-
         <div class="row">
             <div class="col-lg-10 offset-lg-1">
                 <div class="toys__alert">
@@ -102,8 +65,6 @@ Template Name: Игрушки
             </div>
         </div>
     </div>
-</div>
 
 <?php
-    get_footer();
-?>
+get_footer();
